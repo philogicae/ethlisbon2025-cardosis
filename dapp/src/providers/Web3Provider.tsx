d8fe761f8a9fe1ff55494292b64f9016e000e00d
philogicae <38438271+philogicae@users.noreply.github.com>
2025-05-16 22:13:25 +0300

    Fix SIWE

--- dapp/src/providers/Web3Provider.tsx
+++ dapp/src/providers/Web3Provider.tsx
@@ -3,10 +3,10 @@ import { WagmiProvider, createConfig, http } from "wagmi";
 import { gnosis, sepolia } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import {
-  ConnectKitProvider,
-  getDefaultConfig,
-  SIWEConfig,
-  SIWEProvider,
+	ConnectKitProvider,
+	getDefaultConfig,
+	type SIWEConfig,
+	SIWEProvider,
 } from "connectkit";
 import { SiweMessage } from "siwe";
 import { baseApi } from "@/constants/api";
@@ -14,86 +14,89 @@ import axios from "axios";
 import { SIWE_SESSION_ID } from "@/constants/storage";
 
 const config = createConfig(
-  getDefaultConfig({
-    // Your dApps chains
-    chains: [sepolia, gnosis],
-    transports: {
-      // RPC URL for each chain
-      [sepolia.id]: http(`https://eth-sepolia.public.blastapi.io`),
-      [gnosis.id]: http(`https://gnosis-mainnet.public.blastapi.io`),
-    },
-    enableFamily: false,
-    // Required API Keys
-    walletConnectProjectId:
-      process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "",
-    appName: "Cardosis",
-  })
+	getDefaultConfig({
+		// Your dApps chains
+		chains: [sepolia, gnosis],
+		transports: {
+			// RPC URL for each chain
+			[sepolia.id]: http("https://eth-sepolia.public.blastapi.io"),
+			[gnosis.id]: http("https://gnosis-mainnet.public.blastapi.io"),
+		},
+		enableFamily: false,
+		// Required API Keys
+		walletConnectProjectId:
+			process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "",
+		appName: "Cardosis",
+	}),
 );
 
 const queryClient = new QueryClient();
 
 const siweConfig: SIWEConfig = {
-  getNonce: async () =>
-    axios.get(`${baseApi}/siwe/nonce`).then((res) => res.data),
-  createMessage: ({ nonce, address, chainId }) =>
-    new SiweMessage({
-      version: "1",
-      domain: window.location.host,
-      uri: window.location.origin,
-      address,
-      chainId,
-      nonce,
-      // Human-readable ASCII assertion that the user will sign, and it must not contain `\n`.
-      statement: "Sign in With Ethereum.",
-    }).prepareMessage(),
-  verifyMessage: async ({ message, signature }) =>
-    axios
-      .post(
-        `${baseApi}/siwe/verify`,
-        {
-          message,
-          signature,
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      )
-      .then((res) => {
-        localStorage.setItem(SIWE_SESSION_ID, res.data.sessionId);
-        return res.data.ok;
-      }),
-  getSession: async () => {
-    const sessionId = localStorage.getItem(SIWE_SESSION_ID) || "";
+	getNonce: async () =>
+		axios.get(`${baseApi}/siwe/nonce`).then((res) => res.data),
+	createMessage: ({ nonce, address, chainId }) =>
+		new SiweMessage({
+			version: "1",
+			domain: window.location.host,
+			uri: window.location.origin,
+			address,
+			chainId,
+			nonce,
+			// Human-readable ASCII assertion that the user will sign, and it must not contain `\n`.
+			statement: "Sign in With Ethereum.",
+		}).prepareMessage(),
+	verifyMessage: async ({ message, signature }) =>
+		axios
+			.post(
+				`${baseApi}/siwe/verify`,
+				{
+					message,
+					signature,
+				},
+				{
+					headers: {
+						"Content-Type": "application/json",
+					},
+				},
+			)
+			.then((res) => {
+				localStorage.setItem(SIWE_SESSION_ID, res.data.sessionId);
+				return res.data.ok;
+			})
+			.catch(() => {
+				localStorage.removeItem(SIWE_SESSION_ID);
+				return null;
+			}),
+	getSession: async () => {
+		const sessionId = localStorage.getItem(SIWE_SESSION_ID) || "";
+		if (!sessionId) return null;
+		return axios.post(`${baseApi}/siwe/session`, { sessionId }).then((res) => {
+			if (res.data.address) {
+				return res.data;
+			}
+			localStorage.removeItem(SIWE_SESSION_ID);
+			return null;
+		});
+	},
+	signOut: async () => {
+		const sessionId = localStorage.getItem(SIWE_SESSION_ID) || "";
 
-    return axios.post(`${baseApi}/siwe/session`, { sessionId }).then((res) => {
-      if (res.data.address) {
-        return res.data;
-      } else {
-        localStorage.removeItem(SIWE_SESSION_ID);
-        return null;
-      }
-    });
-  },
-  signOut: async () => {
-    const sessionId = localStorage.getItem(SIWE_SESSION_ID) || "";
-
-    return axios.post(`${baseApi}/siwe/logout`, { sessionId }).then((res) => {
-      localStorage.removeItem(SIWE_SESSION_ID);
-      return res.data.ok;
-    });
-  },
+		return axios.post(`${baseApi}/siwe/logout`, { sessionId }).then((res) => {
+			localStorage.removeItem(SIWE_SESSION_ID);
+			return res.data.ok;
+		});
+	},
 };
 
 export const Web3Provider = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <SIWEProvider {...siweConfig}>
-          <ConnectKitProvider>{children}</ConnectKitProvider>
-        </SIWEProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
-  );
+	return (
+		<WagmiProvider config={config}>
+			<QueryClientProvider client={queryClient}>
+				<SIWEProvider {...siweConfig}>
+					<ConnectKitProvider>{children}</ConnectKitProvider>
+				</SIWEProvider>
+			</QueryClientProvider>
+		</WagmiProvider>
+	);
 };
